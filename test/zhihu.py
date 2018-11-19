d82f51bb20d1d9632079de67aa398961935756cf
redmarss <31237613+redmarss@users.noreply.github.com>
2018-11-19 20:57:17 +0800

    test

--- test/zhihu.py
+++ test/zhihu.py
@@ -36,6 +36,8 @@ def get_login_cookie():
     passwd = driver.find_element_by_css_selector('#root > div > main > div > div > div > div.SignContainer-inner > div.Login-content > form > div.SignFlow-password > div.SignFlowInput > div.Input-wrapper > input.Input')
     passwd.send_keys('wuxianwuxian1')
     time.sleep(2)
+    driver.find_element_by_css_selector(
+        '#root > div > main > div > div > div > div.SignContainer-inner > div.Login-content > form > button').click()
     try:
         captcha_input = driver.find_element_by_css_selector('#root > div > main > div > div > div > div > div > form > div.Captcha.SignFlow-captchaContainer > div > div.SignFlowInput > div.Input-wrapper > input.Input')
         captcha = driver.find_element_by_css_selector('#root > div > main > div > div > div > div > div > form > div.Captcha.SignFlow-captchaContainer > div > span.Captcha-englishImage > div.Captcha-englishContainer > img.Captcha-englishImg')
@@ -67,57 +69,8 @@ def get_opener(heads):
     opener.addheaders = header
     return opener
 
-def ungzip(data):
-    try:
-        print("正在解压...")
-        data = gzip.decompress(data)
-        print("解压完成")
-    except:
-        print("无需解压")
-    return data
+
 
 if __name__ == "__main__":
-    ssl._create_default_https_context = ssl._create_unverified_context      #解决弹出可信站点问题
-    heads={
-        "Accept": "*/*",
-        "AcceptLanguage": "zh-CN,zh;q=0.9",
-        "User-Angent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36",
-        "Accept-Encoding":"gzip, deflate",
-        "Host": "www.zhihu.com",
-        "DNT": "1",
-        "Connection": "Keep-Alive"
-    }
+
     cookies_value = get_login_cookie()
-
-    opener = get_opener(heads)
-    url = "https://www.zhihu.com"
-    op = opener.open(url)
-    data1 = op.read()
-    data1 = ungzip(data1).decode('utf-8')
-
-    soup = BeautifulSoup(data1, "html.parser")
-    _xsrf = soup.find("input", {'type':'tel'})
-    _xsrf = _xsrf.get("value")
-    password = "wuxianwuxian1"
-    phone_num = "redmarss@sohu.com"
-    captcha_url = "https://www.zhihu.com/captcha.gif?r=%d&type=login"% (time.time() * 1000)
-    captchadata = opener.open(captcha_url).read()
-
-    yanzhengma = input("captcha:")
-    postdata={
-        "_xsrf":_xsrf,
-        "password":password,
-        "phone_num":phone_num,
-        "captcha":yanzhengma
-    }
-    postdata = urllib.parse.urlencode(postdata).encode()
-    login_url = "https://www.zhihu.com/login/phone_num"
-    op2 = opener.open(login_url, postdata)
-    login_data = op2.read()
-    data = ungzip(login_data).decode("utf-8")
-    print(data)
-    result = dict(eval(data))
-    if result["r"] == 0:
-        print("登录成功")
-    else:
-        print("登录失败")
